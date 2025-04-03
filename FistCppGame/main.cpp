8c3c0b457dbf0469ac6dc2bf25490ee131ca15a7
runefor <fkjy132@gmail.com>
2025-04-03 17:04:15 +0900

    화면의 기본 좌표계 원점으로 세팅하기

--- FistCppGame/main.cpp
+++ FistCppGame/main.cpp
@@ -1,7 +1,8 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QgraphicsScene>
 #include <QtWidgets/QgraphicsRectItem>
-#include <QtWidgets/QgraphicsView.h>
+#include <QtWidgets/QgraphicsView>
+#include <QScreen>
 
 #include<qbrush.h>
 
@@ -10,13 +11,19 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     
+    // QScreen: 현재 화면 정보 가져오기
+    QScreen* screen = QGuiApplication::primaryScreen();
+    QRect screenGeometry = screen->geometry(); // 전체 화면 크기
+
     QGraphicsScene scene;
     scene.setBackgroundBrush(QBrush(Qt::white));
+    scene.setSceneRect(0, 0, screenGeometry.width(), screenGeometry.height()); // 좌표계가 (0,0)에서 시작할 수 있도록 세팅
     QGraphicsRectItem rect(0, 0, 200, 100);
     scene.addItem(&rect);
 
     QGraphicsView view(&scene);
     view.showFullScreen();
+    //view.show();
 
     return a.exec();
 }
